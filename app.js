7587fb183a4c1e57391c67583f739dd9ff455297
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-16 23:26:07 -0600

    fixing proctfile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ===============================DEPENDENCIES======================================
 const express = require('express');
-const path = require("path");
+// const path = require("path");
 const app = express()
 const PORT = process.env.PORT || 5000;
 // ===============================Basic Route======================================
