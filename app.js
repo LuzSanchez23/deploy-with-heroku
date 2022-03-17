2bad523521d592dc5dc7db32bab2120560e02913
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-16 23:07:23 -0600

    adding proctfile

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", function(req, res){
 
 // =====================================Start Server==========================
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
   })
 
