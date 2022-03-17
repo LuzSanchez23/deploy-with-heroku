36888a5bc94a495004c20d2ee24c66bbdb8a856d
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-16 22:38:28 -0600

    deploy with heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require("path");
 const app = express()
-const PORT = 5000
+const PORT = Process.env.PORT || 5000;
 // ===============================Basic Route======================================
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"))
