e87afdbdb2a9dcff15cac3388fe08d90664b1666
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-16 22:31:08 -0600

    creating a express app

---
+++ app.js
@@ -0,0 +1,16 @@
+// ===============================DEPENDENCIES======================================
+const express = require('express');
+const path = require("path");
+const app = express()
+const PORT = 5000
+// ===============================Basic Route======================================
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+// =====================================Start Server==========================
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+  })
+
