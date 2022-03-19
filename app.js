09cad0ab533581856cf36a082b274ed0bfe5a413
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-19 13:57:47 -0600

    testing

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require("path");
 const app = express()
 const PORT = 5000;
+
+// use the express-static middleware
+app.use(express.static("public"))
+
 // ===============================Basic Route======================================
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"))
