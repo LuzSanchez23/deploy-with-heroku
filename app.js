8d723602ad59910260f6b63aa6314b60c35c14fe
Luz Sanchez <nena_baby23@hotmail.com>
2022-03-19 14:39:56 -0600

    fixing error

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require("path");
 const app = express()
 const PORT = 5000;
 
-// // use the express-static middleware
-// app.use(express.static("public"))
+// use the express-static middleware
+app.use(express.static("public"))
 
 // ===============================Basic Route======================================
 app.get("/", function(req, res){
